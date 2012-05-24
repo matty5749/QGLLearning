740d28df12ed852fe1ea9167c9f542ccc80de0e9
matty <j.m.chantrein@free.fr>
2012-05-24 12:44:57 +0200

    Creation de la classe QGLClearColor , base de Gaetan PICOT

---
+++ src/qglclearcolor.cpp
@@ -0,0 +1,36 @@
+/**
+ *\file qglcolor.cpp
+ *\author Jean-Mathieu CHANTREIN
+ *\author Gaetan PICOT
+ *\version 2.0 Copyright CC-BY-NC-SA (Creative Commons) https://fr.wikipedia.org/wiki/Licence_Creative_Commons
+ */
+#include "qglclearcolor.h"
+
+
+#include <QLabel>
+#include <QSpinBox>
+
+QGLClearColor::QGLClearColor(Color4f* color, QWidget* parent): QGlFunction4f(parent), m_color(color)
+{
+    m_nameFunction->setText("glClearColor(");
+
+    m_spin1->setRange(0,99);
+    m_spin2->setRange(0,99);
+    m_spin3->setRange(0,99);
+    m_spin4->setRange(0,99);
+
+    m_spin1->setPrefix("0.");
+    m_spin2->setPrefix("0.");
+    m_spin3->setPrefix("0.");
+    m_spin4->setPrefix("0.");
+
+    m_spin1->setValue(m_color->getR()*100);
+    m_spin2->setValue(m_color->getG()*100);
+    m_spin3->setValue(m_color->getB()*100);
+    m_spin4->setValue(m_color->getA()*100);
+
+    connect(m_spin1,SIGNAL(valueChanged(int)),m_color,SLOT(changeR(int)));
+    connect(m_spin2,SIGNAL(valueChanged(int)),m_color,SLOT(changeG(int)));
+    connect(m_spin3,SIGNAL(valueChanged(int)),m_color,SLOT(changeB(int)));
+    connect(m_spin4,SIGNAL(valueChanged(int)),m_color,SLOT(changeA(int)));
+}
---
+++ src/qglclearcolor.h
@@ -0,0 +1,26 @@
+/**
+ *\file qglcolor.h
+ *\author Jean-Mathieu CHANTREIN
+ *\author Gaetan PICOT
+ *\version 2.0 Copyright CC-BY-NC-SA (Creative Commons) https://fr.wikipedia.org/wiki/Licence_Creative_Commons
+ */
+#ifndef QGLCLEARCOLOR_H
+#define QGLCLEARCOLOR_H
+
+#include "qglfunction4f.h"
+
+#include "color4f.h"
+/**
+ * \class QGLClearColor
+ * \brief Creer un widget permettant la gestion des couleurs du tampon de couleur lors de son effacement
+ * \details http://www.opengl.org/sdk/docs/man/
+ */
+class QGLClearColor : public QGLFunction4f
+{
+private:
+    Color4f* m_color;
+public:
+    QGLClearColor(Color4f* color=0 , QWidget* parent = 0);
+};
+
+#endif // QGLCLEARCOLOR_H
